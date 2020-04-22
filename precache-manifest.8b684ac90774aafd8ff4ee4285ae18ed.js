3128b4a58c4c7215a08c00d1cf7645382a91c338
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2020-04-22 05:40:30 +0100

    live front end move

---
+++ precache-manifest.8b684ac90774aafd8ff4ee4285ae18ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3f15427bc466239b748b1318d67cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "492a164bf897c82f5706",
+    "url": "/static/css/main.b8a55440.chunk.css"
+  },
+  {
+    "revision": "d70ccadeab45cdc5dac3",
+    "url": "/static/js/2.10033851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10033851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492a164bf897c82f5706",
+    "url": "/static/js/main.e1daf2a8.chunk.js"
+  },
+  {
+    "revision": "6ef816ccfc8d1f074ad5",
+    "url": "/static/js/runtime-main.136ba90e.js"
+  },
+  {
+    "revision": "319a401bfb34c4ff38c18f13a0fa3b34",
+    "url": "/static/media/final.319a401b.jpg"
+  }
+]);
